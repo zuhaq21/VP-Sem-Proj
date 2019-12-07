cce42545fe8d07e8b245419a54211b048362241a
zuhaq21 <57126201+zuhaq21@users.noreply.github.com>
2019-12-07 13:05:23 +0500

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.speakButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkButton);
+            this.panel1.Controls.Add(this.speakButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 140);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkButton
+            // 
+            this.checkButton.BackColor = System.Drawing.SystemColors.Control;
+            this.checkButton.Enabled = false;
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(244, 99);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(75, 38);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = false;
+            // 
+            // speakButton
+            // 
+            this.speakButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speakButton.Location = new System.Drawing.Point(148, 99);
+            this.speakButton.Name = "speakButton";
+            this.speakButton.Size = new System.Drawing.Size(75, 38);
+            this.speakButton.TabIndex = 0;
+            this.speakButton.Text = "Speak";
+            this.speakButton.UseVisualStyleBackColor = true;
+            this.speakButton.Click += new System.EventHandler(this.speakButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(83, 158);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(133, 144);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Enabled = false;
+            this.panel3.Location = new System.Drawing.Point(244, 158);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(139, 144);
+            this.panel3.TabIndex = 2;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(359, 307);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 34);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(461, 353);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button speakButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern long mciSendString(string command, StringBuilder retstring, int returnLength, IntPtr callBack);
+        public Form1()
+        {
+            InitializeComponent();
+            mciSendString("open new Type waveaudio alias recsound", null, 0, IntPtr.Zero);
+            speakButton.Click += new EventHandler(this.speakButton_Click);
+
+        }
+        private void buttonclick(object sender, EventHandler e)
+        {
+            //throw new NotImplementedException();
+            mciSendString("record recsound", null, 0, IntPtr.Zero);
+            checkButton.Click += new EventHandler(this.checkButton_Click);
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            speakButton.Enabled = true;
+            checkButton.Enabled = false;
+          //  throw new NotImplementedException();
+            mciSendString("save recsound d:\\zz\\zz.wav", null, 0, IntPtr.Zero);
+            mciSendString("close recsound", null, 0, IntPtr.Zero);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void speakButton_Click(object sender, EventArgs e)
+        {
+            speakButton.Enabled = false;
+            checkButton.Enabled = true;
+            mciSendString("record recsound", null, 0, IntPtr.Zero);
+            checkButton.Click += new EventHandler(this.checkButton_Click);
+        }
+    }
+}
